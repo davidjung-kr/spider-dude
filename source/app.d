14155af4da302b017501cd7a52975c32692b0358
davidjung-kr <jerryjueng@daum.net>
2022-03-28 23:17:53 +0900

    예제코드 변경

--- source/app.d
+++ source/app.d
@@ -23,26 +23,27 @@ void main() {
 	
 	// 1. 거래소 데이터 적재 (2022년 3월 21일 기준)
 	Downloader krxClient = new Downloader();
-	krxClient.readKrxCapAllByBlock(Date(2022, 03, 21), myReport);
+	krxClient.readKrxCapAllByBlock(Date(2022, 03, 28), myReport);
 
-	// 2. 재무상태표를 내 보고서에 적재 (2021_3Q_OFS_BS_20220215.txt → 2021년 3분기 연결 재무제표)
-	Parser bsOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.OFS, StatementType.BS);
+	Parser bsOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.CFS, StatementType.BS);
 	bsOfs2021Y3.read(myReport);
 
-	// 3. 손익계산서를 내 보고서에 적재 (2021_3Q_OFS_IS_20220215.txt → 2021년 3분기 연결 손익계산서)
-	Parser isOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.OFS, StatementType.IS);
+	Parser isOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.CFS, StatementType.IS);
 	isOfs2021Y3.read(myReport);
-
+	
 	// 4. 비정상 종목 필터링
-	//	- 비상장 종목, 상장폐지 종목, 중국회사 제거
-	myReport.filteringOnlyListed();
-	myReport.filteringNotCapZero();
-	myReport.filteringNotChineseCompany();
+	// myReport.filteringOnlyListed();				-- 비상장 종목
+	// myReport.filteringNotCapZero();				-- 상장폐지 종목
+	// myReport.filteringNotChineseCompany();		-- 중국회사 제거
+	myReport.filteringIntersectionCorpCode(); //	-- 재무데이터 있는 종목만 남기기
 
-	// 5. 원하는 밸류에이션 지표 적용
-	/// - 예) PBR이 1 이하 주식 찾기
-	Formula pbrFilter = new Formula(myReport);
-	foreach(stock; pbrFilter.query([FormulaName.PBR])["PBR"]) {
-		writef("[%s] %f\n", stock.code, stock.value);
+	Formula f1 = new Formula(myReport);
+	foreach(per; f1.per()) {
+		writef("[%s] PER: %f\n", per.code, per.ratio);
+	}
+
+	Formula f2 = new Formula(myReport);
+	foreach(evEbita; f1.per()) {
+		writef("[%s] EV/EBITA: %f\n", evEbita.code, evEbita.ratio);
 	}
 }

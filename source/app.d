fd78ea2c922c26b1d743a56c2039ece62f5c03dd
davidjung-kr <jerryjueng@daum.net>
2022-04-01 23:49:01 +0900

    샘플 보고서 추가

--- source/app.d
+++ source/app.d
@@ -9,41 +9,9 @@ module com.davidjung.spider.app;
  * License: GPL-3.0
  */
 
-import std.stdio;
-import std.datetime;
-import com.davidjung.spider.parser;
-import com.davidjung.spider.types;
-import com.davidjung.spider.formula;
-import com.davidjung.spider.report;
-import com.davidjung.spider.downloader;
+extern(Windows) int SetConsoleOutputCP(uint);
 
 void main() {
-	// 내 보고서
-	Report myReport = new Report();
-	
-	// 1. 거래소 데이터 적재 (2022년 3월 21일 기준)
-	Downloader krxClient = new Downloader();
-	krxClient.readKrxCapAllByBlock(Date(2022, 03, 28), myReport);
-
-	Parser bsOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.CFS, StatementType.BS);
-	bsOfs2021Y3.read(myReport);
-
-	Parser isOfs2021Y3 = new Parser("2021", Period.Y3, ReportType.CFS, StatementType.IS);
-	isOfs2021Y3.read(myReport);
-	
-	// 4. 비정상 종목 필터링
-	// myReport.filteringOnlyListed();				-- 비상장 종목
-	// myReport.filteringNotCapZero();				-- 상장폐지 종목
-	// myReport.filteringNotChineseCompany();		-- 중국회사 제거
-	myReport.filteringIntersectionCorpCode(); //	-- 재무데이터 있는 종목만 남기기
-
-	Formula f1 = new Formula(myReport);
-	foreach(per; f1.per()) {
-		writef("[%s] PER: %f\n", per.code, per.ratio);
-	}
-
-	Formula f2 = new Formula(myReport);
-	foreach(evEbita; f1.per()) {
-		writef("[%s] EV/EBITA: %f\n", evEbita.code, evEbita.ratio);
-	}
+	if(SetConsoleOutputCP(65001) == 0)
+        throw new Exception("Failed");
 }

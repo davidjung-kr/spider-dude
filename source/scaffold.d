56844bef9c197f0e864e875304f25ab9320c8489
davidjung-kr <jerryjueng@daum.net>
2022-04-05 23:55:49 +0900

    샘플 보고서 추가(GP/A)

--- source/scaffold.d
+++ source/scaffold.d
@@ -22,7 +22,7 @@ class NetNetStocks {
 	this(Period period, ReportType reportType) {
 		Report myReport = new Report();
 		Downloader krxClient = new Downloader();
-		krxClient.readKrxCapAllByBlock(Date(2022, 03, 31), myReport);
+		krxClient.readKrxCapAllByBlock(Date(2022, 04, 04), myReport);
 
 		Parser bsOfs2021Y4 = new Parser("2021", period, reportType, StatementType.BS);
 		bsOfs2021Y4.read(myReport);
@@ -68,5 +68,90 @@ class NetNetStocks {
 				ncav.value
 				);
 		}
+	}
+}
+
+class GpaStocks {
+	this(Period period, ReportType reportType) {
+		Report myReport = new Report();
+		Downloader krxClient = new Downloader();
+		krxClient.readKrxCapAllByBlock(Date(2022, 04, 04), myReport);
+
+		Parser bsOfs2021Y4 = new Parser("2021", period, reportType, StatementType.BS);
+		bsOfs2021Y4.read(myReport);
+
+		Parser isOfs2021Y4 = new Parser("2021", period, reportType, StatementType.CIS);
+		isOfs2021Y4.read(myReport);
+		
+		// 4. 비정상 종목 필터링
+		myReport.filteringOnlyListed();			   // 비상장 종목
+		myReport.filteringNotCapZero();			   // 상장폐지 종목
+		myReport.filteringNotChineseCompany();     // 중국회사 제거
+		myReport.filteringIntersectionCorpCode();  // 재무데이터 있는 종목만 남기기
+		
+		Formula f1 = new Formula(myReport);
+		auto result = f1.query([FormulaName.GP_A, FormulaName.PBR, FormulaName.PER]);
+		auto gpas = result["GP/A"];
+		auto pbrs = result["PBR"];
+		auto pers = result["PER"];
+
+		string current = Clock.currTime().toISOString();
+		File ff = File("GpaStocks_"~current~".txt", "w");
+
+		
+		ff.writeln("2021 / "~GetCodeFrom.period(period)~" / ["~GetCodeFrom.reportType(reportType)~"]");
+		ff.writeln("종목코드\t종목명\t자산총계\t매출총이익\tGP/A\tPBR\tPER");
+		for(int i=0; i<gpas.length; i++) {
+			FormulaResult pbr = pbrs[i];
+			FormulaResult gpa = gpas[i];
+			FormulaResult per = pers[i];
+
+			Cis income = myReport.getComprehensiveIncomeStatement(pbr.code);
+			Bs balance = myReport.getBalanceStatement(pbr.code);
+
+			ff.writef("'%s\t%s\t%d\t%d\t%f\t%f\t%f\n",
+				pbr.code,
+				myReport.getCorpName(pbr.code),
+				balance.q(IfrsCode.FULL_ASSETS),
+				income.q(IfrsCode.FULL_GROSSPROFIT),
+				gpa.ratio,
+				pbr.ratio,
+				per.ratio
+			);
+		}
+	}
+}
+
+class LowPerStocks {
+	this(ReportType reportType) {
+		Report[string] reports = [
+			"2017":new Report(),
+			"2018":new Report(),
+			"2019":new Report(),
+			"2020":new Report(),
+			"2021":new Report(),
+		];
+		/*
+		Downloader krxClient = new Downloader();
+		krxClient.readKrxCapAllByBlock(Date(2018, 04, 04), reports["2018"]);
+		krxClient.readKrxCapAllByBlock(Date(2019, 04, 04), reports["2019"]);
+		krxClient.readKrxCapAllByBlock(Date(2020, 04, 03), reports["2020"]);
+		krxClient.readKrxCapAllByBlock(Date(2021, 04, 02), reports["2021"]);
+
+		foreach(year; reports.keys) {
+			Parser forBalance = new Parser(year, Period.Y4, reportType, StatementType.BS);
+			Parser forIncome = new Parser(year, Period.Y4, reportType, StatementType.CIS);
+			forBalance.read(reports[year]);
+			forIncome.read(reports[year]);
+
+			reports[year].filteringOnlyListed(); // 비상장 종목
+			reports[year].filteringNotChineseCompany();     // 중국회사 제거
+			reports[year].filteringIntersectionCorpCode();  // 재무데이터 있는 종목만 남기기
+		}
+
+		foreach(reports["2017"].getListShared()) {
+		
+			Cis cis = reports[year].getComprehensiveIncomeStatement();	
+		}*/
 	}
 }

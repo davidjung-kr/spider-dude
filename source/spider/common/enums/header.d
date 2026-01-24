d3b957e708c86e38868faec54e314eee235fec92
davidjung-kr <zerohunj@gmail.com>
2026-01-24 23:50:59 +0900

    UA 뒤에 버전넘버 추가

--- source/spider/common/enums/header.d
+++ source/spider/common/enums/header.d
@@ -1,6 +1,8 @@
 module spider.common.enums.header;
 
 enum UserAgent {
-    W64_10_MZ_FFOX = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
-    W64_10_MS_EDGE = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0"
+    W64_10_MZ_FFOX_145 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:145.0) Gecko/20100101 Firefox/145.0",
+    W64_10_MZ_FFOX_147 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:147.0) Gecko/20100101 Firefox/147.0",
+    W64_10_MS_EDGE_142 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36 Edg/142.0.0.0",
+    W64_10_MS_EDGE_144 = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.0.0 Safari/537.36 Edg/144.0.0.0"
 }
